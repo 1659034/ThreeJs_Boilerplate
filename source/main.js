a0b44d1609d0d4e33e5050a59be626fcd10eb5be
Y Do <ydo@fromlabs.com>
2021-05-25 10:26:49 +0700

    resource PATH

--- source/main.js
+++ source/main.js
@@ -1,8 +1,8 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
+import * as THREE from '../build/three.module.js';
 
-import {FBXLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/FBXLoader.js';
-import {GLTFLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/GLTFLoader.js';
-import {OrbitControls} from 'https://cdn.jsdelivr.net/npm/three@0.118/examples/jsm/controls/OrbitControls.js';
+import {FBXLoader} from '../jsm/loaders/FBXLoader.js';
+import {GLTFLoader} from '../jsm/loaders/FBXLoader.js';
+import {OrbitControls} from '../jsm/controls/OrbitControls.js';
 
 
 class BasicCharacterControls {
